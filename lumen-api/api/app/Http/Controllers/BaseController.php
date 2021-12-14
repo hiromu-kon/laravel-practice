6840b7229429862f7d692610e2ae08aef541f627
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-12-14 23:16:21 +0900

    fix バリデーション処理

--- lumen-api/api/app/Http/Controllers/BaseController.php
+++ lumen-api/api/app/Http/Controllers/BaseController.php
@@ -24,16 +24,6 @@ class BaseController extends Controller
     public static function validation($request, $data, $customMessages = [])
     {
 
-        $response = array(
-            "success"   => true,
-            "message"   => ""
-        );
-
-        if (count($data) <= 0) {
-
-            return $response;
-        }
-
         $params = $request->all();
 
         $validator = \Validator::make($params, $data, $customMessages);
@@ -45,29 +35,9 @@ class BaseController extends Controller
             ],
         ];
 
-        throw new HttpResponseException(response()->json($data, 422));
+        return $data;
 
-        if ($validator->fails()) {
+        // throw new HttpResponseException(response()->json($data, 422));
 
-            $params     = [];
-            $messages   = $validator->messages();
-
-            foreach(json_decode($messages) as $key => $message) {
-
-                $details = [];
-
-                foreach($message as $detail) {
-
-                    $details[] = $detail;
-                }
-
-                $params[] = (count($customMessages) > 0 ? "" : $key . ":") . implode("|", $details);
-            }
-
-            $response["success"] = false;
-            $response["message"] = implode("</br>", $params);
-        }
-
-        return $response;
     }
 }
