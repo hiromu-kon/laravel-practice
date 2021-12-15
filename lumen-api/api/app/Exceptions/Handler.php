9ab05793e4f57a3050e845b9b820320101ab5a0a
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-12-15 23:11:44 +0900

    fix: エラー処理

--- lumen-api/api/app/Exceptions/Handler.php
+++ lumen-api/api/app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Database\QueryException;
 use App\Traits\JsonRespondController;
 use Throwable;
 
@@ -68,5 +69,75 @@ class Handler extends ExceptionHandler
 
 
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Get the json response for the exception.
+     *
+     * @param Exception $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function getJsonResponse(Exception $exception)
+    {
+        $debugEnabled = config('app.debug');
+
+        $exception = $this->prepareException($exception);
+
+        /*
+         * Handle validation errors thrown using ValidationException.
+         */
+        if ($exception instanceof ValidationException) {
+
+            $validationErrors = $exception->validator->errors()->getMessages();
+
+            $validationErrors = array_map(function($error) {
+                return array_map(function($message) {
+                    return remove_words($message, 2);
+                }, $error);
+            }, $validationErrors);
+
+            return response()->json(['errors' => $validationErrors], 422);
+        }
+
+        /*
+         * Handle database errors thrown using QueryException.
+         * Prevent sensitive information from leaking in the error message.
+         */
+        if ($exception instanceof QueryException) {
+            if ($debugEnabled) {
+                $message = $exception->getMessage();
+            } else {
+                $message = 'Internal Server Error';
+            }
+        }
+
+        $statusCode = $this->getStatusCode($exception);
+
+        if (! isset($message) && ! ($message = $exception->getMessage())) {
+            $message = sprintf('%d %s', $statusCode, Response::$statusTexts[$statusCode]);
+        }
+
+        $errors = [
+            'message' => $message,
+            'status_code' => $statusCode,
+        ];
+
+        if ($debugEnabled) {
+            $errors['exception'] = get_class($exception);
+            $errors['trace'] = explode("\n", $exception->getTraceAsString());
+        }
+
+        return response()->json(['errors' => $errors], $statusCode);
+    }
+
+    /**
+     * Get the status code from the exception.
+     *
+     * @param \Exception $exception
+     * @return int
+     */
+    protected function getStatusCode(Exception $exception)
+    {
+        return $this->isHttpException($exception) ? $exception->getStatusCode() : 500;
     }
 }
